dc1290bfade08206fbaec47076475ae0b95990f4
naiem <naiemhosen430@gmail.com>
2023-06-16 11:15:50 +0600

    1st commit

---
+++ It's UTSHO/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Menu from "./components/Home"
+
+function App() {
+
+  return (
+    <>
+      <Menu />
+    </>
+  )
+}
+
+export default App
---
+++ It's UTSHO/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Menu () {
+    return (
+        <>
+        <section>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Project</a></li>
+                <li><a href="">Contract</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+        </section>
+        </>
+    )
+}
+
+export default Menu
